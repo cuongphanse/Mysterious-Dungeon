d412e266c6c9c56b52af71a2786bdc87aafbc3eb
cuongphan <cuongpvpvc@gmail.com>
2024-03-07 21:27:40 +0700

    Item,Inventory,EquipItem,UnEquip,Craft

--- Assets/Scripts/Stats/EnemyStat.cs
+++ Assets/Scripts/Stats/EnemyStat.cs
@@ -5,10 +5,49 @@ using UnityEngine;
 public class EnemyStat : CharacterStats
 {
     private Enemy enemy;
+
+    [Header("Level details")]
+    [SerializeField] private int level = 1;
+
+    [Range(0f, 1f)]
+    [SerializeField] private float percantageModifier =.4f;
     protected override void Start()
     {
+        ApplyLevelModifier();
         base.Start();
         enemy = GetComponent<Enemy>();
+
+    }
+
+    private void ApplyLevelModifier()
+    {
+        Modify(strengh);
+        Modify(agility);
+        Modify(intelligence);
+        Modify(vitality);
+
+        Modify(damage);
+        Modify(critRate);
+        Modify(critDamage);
+
+        Modify(maxHealth);
+        Modify(armor);
+        Modify(evasion);
+        Modify(magicResistance);
+
+        Modify(fireDamage);
+        Modify(iceDamage);
+        Modify(lightningDamage);
+    }
+
+    private void Modify(Stat _stat)
+    {
+        for (int i = 1; i< level; i++)
+        {
+            float modifier = _stat.GetValue() * percantageModifier;
+
+            _stat.AddModifier(Mathf.RoundToInt(modifier));
+        }
     }
 
     public override void TakeDamage(int _damage)
--- Assets/Scripts/Stats/Stat.cs
+++ Assets/Scripts/Stats/Stat.cs
@@ -29,6 +29,6 @@ public class Stat
     }
     public void RemoveModifier(int _modifier)
     {
-        modifiers.RemoveAt(_modifier);
+        modifiers.Remove(_modifier);
     }
 }

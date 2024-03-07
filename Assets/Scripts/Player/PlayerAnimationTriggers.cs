28dcf35a11fb217cc400990da421d7d9f3a73e4e
cuongphan <cuongpvpvc@gmail.com>
2024-03-08 03:25:40 +0700

    Aseet door

--- Assets/Scripts/Player/PlayerAnimationTriggers.cs
+++ Assets/Scripts/Player/PlayerAnimationTriggers.cs
@@ -20,7 +20,7 @@ public class PlayerAnimationTriggers : MonoBehaviour
                 EnemyStat _target = hit.GetComponent<EnemyStat>();
                 player.stats.DoDamage(_target);
 
-                Inventory.instance.GetEquipment(EquipmentType.Weapon).ExecuteItemEffect();
+                //Inventory.instance.GetEquipment(EquipmentType.Weapon).ExecuteItemEffect();
             }
         }
     }
